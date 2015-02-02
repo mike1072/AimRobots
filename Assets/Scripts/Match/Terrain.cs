dd4146b30a61153a139ce20393278bba68c7d9da
Mike <mike1072@gmail.com>
2015-02-01 21:28:00 -0800

    started work on a real terrain system

---
+++ Assets/Scripts/Match/Terrain.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terrain : MonoBehaviour
+{
+    Texture2D texture;
+
+	// Use this for initialization
+	void Start()
+    {
+        texture = gameObject.GetComponent<SpriteRenderer>().sprite.texture;
+        HighlightEdges();
+	}
+
+    void HighlightEdges()
+    {
+        for (int x = 0; x < texture.width; x++)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                if (PixelIsSolid(x, y) == true && PixelTouchingAir(x, y))
+                {
+                    texture.SetPixel(x, y, Color.black);
+                }
+            }
+        }
+        texture.Apply();
+    }
+
+    bool PixelTouchingAir(int x, int y)
+    {
+        if (PixelIsSolid(x - 1, y) == false)
+            return true;
+        if (PixelIsSolid(x + 1, y) == false)
+            return true;
+        if (PixelIsSolid(x, y - 1) == false)
+            return true;
+        if (PixelIsSolid(x, y + 1) == false)
+            return true;
+        if (PixelIsSolid(x - 1, y - 1) == false)
+            return true;
+        if (PixelIsSolid(x - 1, y + 1) == false)
+            return true;
+        if (PixelIsSolid(x + 1, y - 1) == false)
+            return true;
+        if (PixelIsSolid(x + 1, y + 1) == false)
+            return true;
+
+        return false;
+    }
+
+    bool? PixelIsSolid(int x, int y)
+    {
+        if (x < 0 || x >= texture.width || y < 0 || y >= texture.height)
+            return null;
+
+        return texture.GetPixel(x, y).a > 0.5f;
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+
+    void OnMouseDown()
+    {
+
+    }
+}
