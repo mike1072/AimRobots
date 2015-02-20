1650b429ef4796cf5c55b0b77900aad3d46ea22a
Mike <mike1072@gmail.com>
2015-02-20 02:58:26 -0800

    fixed sprite load; added click to highlight ground

---
+++ Assets/Scripts/IntVector2.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public struct IntVector2
+{
+    public int x;
+    public int y;
+
+    public IntVector2(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public IntVector2(float x, float y)
+    {
+        this.x = (int)x;
+        this.y = (int)y;
+    }
+
+    public IntVector2(Vector2 v) : this(v.x, v.y) { }
+
+    #region operator overloads
+    public static IntVector2 operator -(IntVector2 a)
+    {
+        return new IntVector2(-a.x, -a.y);
+    }
+
+    public static IntVector2 operator -(IntVector2 a, IntVector2 b)
+    {
+        return new IntVector2(a.x - b.x, a.y - b.y);
+    }
+
+    public static IntVector2 operator +(IntVector2 a, IntVector2 b)
+    {
+        return new IntVector2(a.x + b.x, a.y + b.y);
+    }
+
+    public static bool operator ==(IntVector2 a, IntVector2 b)
+    {
+        return (a.x == b.x && a.y == b.y);
+    }
+
+    public override bool Equals(object a)
+    {
+        if (a == null)
+            return false;
+
+        if (!(a is IntVector2))
+            return false;
+
+        return this.Equals((IntVector2)a);
+    }
+
+    public bool Equals(IntVector2 a)
+    {
+        if ((object)a == null)
+            return false;
+
+        return a.x == this.x && a.y == this.y;
+    }
+
+    public override int GetHashCode()
+    {
+        return this.x.GetHashCode() * 17 + this.y.GetHashCode();
+    }
+
+    public static bool operator !=(IntVector2 a, IntVector2 b)
+    {
+        return !(a == b);
+    }
+
+    public static Vector2 operator -(Vector2 a, IntVector2 b)
+    {
+        return new Vector2(a.x - b.x, a.y - b.y);
+    }
+
+    public static Vector2 operator -(IntVector2 a, Vector2 b)
+    {
+        return new Vector2(a.x - b.x, a.y - b.y);
+    }
+
+    public static Vector2 operator +(Vector2 a, IntVector2 b)
+    {
+        return new Vector2(a.x + b.x, a.y + b.y);
+    }
+
+    public static Vector2 operator +(IntVector2 a, Vector2 b)
+    {
+        return (b + a);
+    }
+
+    public static bool operator ==(Vector2 a, IntVector2 b)
+    {
+        return (a.x == b.x && a.y == b.y);
+    }
+
+    public static bool operator ==(IntVector2 a, Vector2 b)
+    {
+        return (b == a);
+    }
+
+    public static bool operator !=(Vector2 a, IntVector2 b)
+    {
+        return !(a == b);
+    }
+
+    public static bool operator !=(IntVector2 a, Vector2 b)
+    {
+        return (b != a);
+    }
+    #endregion
+
+    // implicit conversion to Vector2
+    public static implicit operator Vector2(IntVector2 a)
+    {
+        return new Vector2(a.x, a.y);
+    }
+}
--- Assets/Scripts/Match/Terrain.cs
+++ Assets/Scripts/Match/Terrain.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Terrain : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
     Texture2D texture;
+    Vector2 textureDimensions;
 
 	// Use this for initialization
 	void Start()
     {
-        texture = gameObject.GetComponent<SpriteRenderer>().sprite.texture;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        texture = Instantiate(spriteRenderer.sprite.texture) as Texture2D;
+        spriteRenderer.sprite = Sprite.Create(texture, spriteRenderer.sprite.rect, new Vector2(0.5f, 0.5f), spriteRenderer.sprite.pixelsPerUnit);
+        
+        textureDimensions = new Vector2(texture.width, texture.height);
         HighlightEdges();
 	}
 
@@ -18,53 +25,110 @@ public class Terrain : MonoBehaviour
         {
             for (int y = 0; y < texture.height; y++)
             {
-                if (PixelIsSolid(x, y) == true && PixelTouchingAir(x, y))
+                if (PixelOnMap(x,y) && PixelIsSolid(x, y))
                 {
-                    texture.SetPixel(x, y, Color.black);
+                    var airPixels = GetNearbyAirPixels(x, y);
+                    if (airPixels.Count > 0)
+                    {
+                        texture.SetPixel(x, y, Color.black);
+                        Color halfTransparent = Color.black;
+                        halfTransparent.a = 0.25f;
+                        foreach (var pixel in airPixels)
+                        {
+                            texture.SetPixel(pixel.x, pixel.y, halfTransparent);
+                        }
+                    }
+                    if (PixelIsGround(x, y))
+                    {
+                        texture.SetPixel(x, y, Color.red);
+                    }
                 }
             }
         }
         texture.Apply();
     }
 
-    bool PixelTouchingAir(int x, int y)
+    static List<IntVector2> nearbyOffsets = new List<IntVector2>()
     {
-        if (PixelIsSolid(x - 1, y) == false)
-            return true;
-        if (PixelIsSolid(x + 1, y) == false)
-            return true;
-        if (PixelIsSolid(x, y - 1) == false)
-            return true;
-        if (PixelIsSolid(x, y + 1) == false)
-            return true;
-        if (PixelIsSolid(x - 1, y - 1) == false)
-            return true;
-        if (PixelIsSolid(x - 1, y + 1) == false)
-            return true;
-        if (PixelIsSolid(x + 1, y - 1) == false)
-            return true;
-        if (PixelIsSolid(x + 1, y + 1) == false)
-            return true;
+            new IntVector2(-1, 0),
+            new IntVector2(1, 0),
+            new IntVector2(0, -1),
+            new IntVector2(0, 1),
+    };
 
-        return false;
+    List<IntVector2> GetNearbyAirPixels(int x, int y)
+    {
+        List<IntVector2> airPixels = new List<IntVector2>();
+
+        foreach (var offset in nearbyOffsets)
+        {
+            var pixel = new IntVector2(x + offset.x, y + offset.y);
+            if (PixelOnMap(x, y) && !PixelIsSolid(pixel.x, pixel.y))
+            {
+                airPixels.Add(pixel);
+            }
+        }
+
+        return airPixels;
     }
 
-    bool? PixelIsSolid(int x, int y)
+    bool PixelIsGround(int x, int y)
     {
-        if (x < 0 || x >= texture.width || y < 0 || y >= texture.height)
-            return null;
+        return PixelOnMap(x, y + 1) && !PixelIsSolid(x, y + 1);
+    }
 
-        return texture.GetPixel(x, y).a > 0.5f;
+    bool PixelIsSolid(int x, int y)
+    {
+        return GetPixelTransparency(x, y) > 0.5f;
+    }
+
+    bool PixelOnMap(int x, int y)
+    {
+        return (x > 0 || x <= texture.width) && (y > 0 || y <= texture.height);
+    }
+
+    int GetGroundBelow(int x, int y)
+    {
+        for (int y0 = y; y0 > 0; y0--)
+        {
+            if (PixelIsSolid(x, y0))
+            {
+                return y0;
+            }
+        }
+        return -1;
+    }
+
+
+    float GetPixelTransparency(int x, int y)
+    {
+        return texture.GetPixel(x, y).a;
     }
 	
 	// Update is called once per frame
 	void Update()
     {
-	
+
 	}
 
-    void OnMouseDown()
+    void HighlightClickedGround()
     {
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (!hit)
+            return;
 
+        IntVector2 localPosition = new IntVector2((Vector2)transform.InverseTransformPoint(hit.point) + textureDimensions/2);
+        var groundY = GetGroundBelow((int)localPosition.x, localPosition.y);
+
+        if (groundY > -1)
+        {
+            texture.SetPixel(localPosition.x, groundY, Color.green);
+            texture.Apply();
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        HighlightClickedGround();
     }
 }
